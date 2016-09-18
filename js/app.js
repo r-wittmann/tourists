a5ebb70130de624a5d0679db1fd9291f13e0d6cb
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-09-18 16:06:39 +0200

    get browsers geolocation and save to scope

--- js/app.js
+++ js/app.js
@@ -6,9 +6,18 @@ tourists.config(['$logProvider', function ($logProvider) {
   // $logProvider.debugEnabled(false)
 }])
 
-tourists.controller('Controller', ['$scope', '$http', 'config', function ($scope, $http, config) {
+tourists.controller('Controller', ['$scope', '$http', 'config', 'LocationService', function ($scope, $http, config, LocationService) {
   function initialize () {
     angular.extend($scope, config)
+    LocationService.getGeolocation($scope.model)
   }
   initialize()
+}])
+
+tourists.service('LocationService', ['$timeout', function ($timeout) {
+  this.getGeolocation = function (model) {
+    navigator.geolocation && navigator.geolocation.watchPosition((position) => {
+      $timeout(() => { model.currentPosition = position }, 0)
+    })
+  }
 }])
